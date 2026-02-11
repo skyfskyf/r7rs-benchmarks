cfdff60d0bb013ef5561c2d67b56f2a743788fad
skyfskyf <bakhmetstephen@gmail.com>
2026-02-11 05:42:44 -0800

    Remove implementation name and values-proc
    Removed implementation name and values-proc definition.

--- src/Grift-prelude.scm
+++ src/Grift-prelude.scm
@@ -5,11 +5,3 @@
   (syntax-rules ()
     ((import . rest) (begin))))
 
-;; Implementation name (update version when upgrading grift dependency)
-(define (this-scheme-implementation-name) "grift-1.4.0")
-
-;; Make values a first-class procedure (grift treats values as a special form)
-(define values-proc (lambda args
-  (if (= (length args) 1)
-      (car args)
-      (apply values args))))
